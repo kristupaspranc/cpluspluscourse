e8f7df1c872e2c186fb2f8f7499787ee092bb9fb
kristupaspranc <kristupas.pranckietis@cern.ch>
2024-03-26 17:58:39 +0100

    polymorph

--- exercises/polymorphism/trypoly.cpp
+++ exercises/polymorphism/trypoly.cpp
@@ -3,13 +3,17 @@
 
 int main() {
     // create a Pentagon, call its perimeter method
-
+    Pentagon myPenty{5};
+    std::cout << myPenty.computePerimeter() << "\n";
 
     // create a Hexagon, call its perimeter method
-
+    Hexagon myHexy{5};
+    std::cout << myHexy.computePerimeter() << "\n";
 
     // create a Hexagon, call the perimeter method through a reference to Polygon
-
+    Hexagon myOtherHexy{5};
+    RegularPolygon & myOtherHexyReferenced{myOtherHexy};
+    std::cout << myOtherHexyReferenced.computePerimeter() << "\n";
 
     // retry virtual method
 
